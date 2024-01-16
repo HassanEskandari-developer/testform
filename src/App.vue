f9cf5876c091435fc7f26f08f26875b55775ba00
Hassan <128363210+HassanEskandari-developer@users.noreply.github.com>
2024-01-16 16:56:21 +0330

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default{
+  data(){
+return{
+  test:"",
+  signInPayload:{
+mobile:'',
+password:''
+  }
+}
+  },
+  methods:{
+    signIn(){
+    console.log(this.signInPayload)
+  }
+  }
+ 
+}
+</script>
+
+<template>
+  
+  <main class="bg-black">
+    <input type="text" v-model="signInPayload.mobile">
+    <input type="text" v-model="signInPayload.password">
+
+    <button type="button" @click="signIn">click me</button>  
+  
+  </main>
+</template>
+
+<style scoped>
+
+</style>
